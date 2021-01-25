cd411e87b102ddb56108fa7196540f331728b3d4
Nina Jade <jaderiver64@gmail.com>
2021-01-24 23:47:04 -0600

    Adding two JS files for our Air Quality and Covid-9 API access

---
+++ assets/air-quality-script.js
@@ -0,0 +1,8 @@
+// Cookie settings for cross-site access
+document.cookie = 'cookie1=value1; SameSite=Lax';
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
+var performSearch = function(searchInput) {
+
+
+}
---
+++ assets/covid-data-script.js
@@ -0,0 +1,3 @@
+// Cookie settings for cross-site access
+document.cookie = 'cookie1=value1; SameSite=Lax';
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
