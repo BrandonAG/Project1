67e3e819c4f3b8c8794701f605b6e1e60369ae20
Nina Jade <jaderiver64@gmail.com>
2021-01-25 16:32:39 -0600

    building AQI information from API to be accessed in JavaScript

--- assets/aqi-covid/script-aqi.js
+++ assets/aqi-covid/script-aqi.js
@@ -2,7 +2,7 @@
 document.cookie = 'cookie1=value1; SameSite=Lax';
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
 // https://api.airvisual.com/v2/nearest_city?key=40f410cd-9102-4b7b-9aed-cdbbce23a985
-
+// 3812ea6836536b0581712ffd66f54fa5
 
 
 var getCurrentAirInfo = function() {
@@ -12,7 +12,7 @@ var getCurrentAirInfo = function() {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-                getWeather(data);
+                getAQIInformation(data);
             });
         } else {
             alert("Error: " + response.statusText);
@@ -21,14 +21,16 @@ var getCurrentAirInfo = function() {
     });
 };
 
-var getWeather = function(results) {
+
+var getAQIInformation = function(results) {
+    getPollutant(results);
     var contentEl = document.getElementById("content");
     var iconIdEl = results.data.current.weather.ic;
     var link = "https://openweathermap.org/img/wn/" + iconIdEl + "@2x.png";;
     var imgCode = "<img src='" + link + "' alt='icon'>";
 
     var aqiBadgeElement = createBadge(results.data.current.pollution.aqius);
-    /* Displayed this way for testing purposes only */
+    /* Displayed this way for testing purposes only so I can see my output */
     contentEl.innerHTML = "<div><h1>Ahoy, " + results.data.city + "!</h1></div><div>" + imgCode + "</div><br>Temp: " +
         convertToF(results.data.current.weather.tp) + "F" +
         "<br><br><div><div>Current AQI (US):</div><div> " + aqiBadgeElement + "</div></div>" +
@@ -57,5 +59,18 @@ var createBadge = function(aqiValue) {
 
 function convertToF(celsius) {
     return celsius * 9 / 5 + 32;
-}
+};
+var getPollutant = function(data) {
+    // Main pollutant for US AQI
+    var mainUS = data.pollution.mainus;
+    // Turn pollutant conde into a human readable string
+    var pollutantString = getPollutantString(mainUS);
+    // Pollutant concentration
+    var concentration = data.pollution.conc;
+    var units = getUnits(mainUS);
+    console.log(unitValue);
+};
+
+var getUnits = function(pollutantCode) {}
+
 getCurrentAirInfo();
