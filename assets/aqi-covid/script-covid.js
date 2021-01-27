26be58e9b7a101c2b1aabaf42fcdb8602d267d89
Nina Jade <jaderiver64@gmail.com>
2021-01-27 00:19:42 -0600

    A few notes, but ready to build this up

--- assets/aqi-covid/script-covid.js
+++ assets/aqi-covid/script-covid.js
@@ -2,10 +2,11 @@
 document.cookie = 'cookie1=value1; SameSite=Lax';
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
 
-// So far worksd good!  Will get this using the fips
-// API KEY: https://api.covidactnow.org/v2/county/55060.json?apiKey=445bc14aef9b4a7798f42f69d834218d
+// So far worked good!  Will get this using the fips
+// API KEY: https://api.covidactnow.org/v2/county/{{FIPS}}.json?apiKey=445bc14aef9b4a7798f42f69d834218d
 var attempt = function() {
-    var apiUrl = "https://api.covidactnow.org/v2/state/MN.json?apiKey=445bc14aef9b4a7798f42f69d834218d";
+    // this works great, tons of data I know we can use.  Also a risk guide: https://globalepidemics.org/wp-content/uploads/2020/09/key_metrics_and_indicators_v5-1.pdf
+    var apiUrl = "https://api.covidactnow.org/v2/county/49017.json?apiKey=445bc14aef9b4a7798f42f69d834218d";
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(dataResult) {
