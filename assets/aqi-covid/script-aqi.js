e49cd4a2e29676f77b66e3f39697716bd80db9b1
Nina Jade <jaderiver64@gmail.com>
2021-01-26 19:48:33 -0600

    latest in the AQI scrpt

--- assets/aqi-covid/script-aqi.js
+++ assets/aqi-covid/script-aqi.js
@@ -2,36 +2,68 @@
 document.cookie = 'cookie1=value1; SameSite=Lax';
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
 // https://api.airvisual.com/v2/nearest_city?key=40f410cd-9102-4b7b-9aed-cdbbce23a985
-// 3812ea6836536b0581712ffd66f54fa5
-
-// Fetch the API data
+// 40f410cd-9102-4b7b-9aed-cdbbce23a985
+var cityArray = [];
+// Fetch the API data for current Air Quality
 var getCurrentAirInfo = function() {
     var apiUrl = "https://api.airvisual.com/v2/nearest_city?key=40f410cd-9102-4b7b-9aed-cdbbce23a985";
     fetch(apiUrl).then(function(response) {
 
         if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-                getAQIInformation(data);
+            response.json().then(function(dataResult) {
+                console.log(dataResult);
+                console.log(dataResult.data.location.coordinates);
+                displayAQIInformation(dataResult);
+
             });
         } else {
             alert("Error: " + response.statusText);
-
         }
     });
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch("http://api.airvisual.com/v2/nearest_city?lat=35.98&lon=140.33&key=40f410cd-9102-4b7b-9aed-cdbbce23a985", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
 };
 
+
+function searchButtonHandler(input) {
+    console.log(input);
+    var apiUrl = "https://api.opencagedata.com/geocode/v1/json?q=" +
+        input + "&key=974cf3d56a9f45d58e79a7ec8b1f7842";
+    // Geocode API - Open cage Data
+    fetch(apiUrl).then(function(response) {
+
+        if (response.ok) {
+            response.json().then(function(geoData) {
+                console.log(geoData)
+                    // Plug the response data into the next function - getWeather()
+            });
+        } else {
+            alert("Error: " + response.statusText);
+            // Check for problems
+        }
+    });
+    //results[0].geometry.lat, results[0].geometry.lng
+};
 // Finds and saves the data needed from the API
-var getAQIInformation = function(results) {
+var displayAQIInformation = function(results) {
+
     /* NOTE: This data is displayed this way (innerHTML) for testing purposes only - this way I can see my output.
     the plan is to build information objects that can be saved and accessed as needed. */
-
+    var cityFormatted = results.data.city + ", " + results.data.state + ", " + results.data.country;
     var headerEl = document.getElementById("current-conditions-header");
-    headerEl.innerHTML = "<div><h1>Ahoy, " + results.data.city + "!</h1></div>";
+    headerEl.innerHTML = "<div><h1>Ahoy, " + cityFormatted + "!</h1></div>";
 
     var contentEl = document.getElementById("content");
     var iconIdEl = results.data.current.weather.ic;
-    var link = "https://openweathermap.org/img/wn/" + iconIdEl + "@2x.png";;
+    var link = "https://openweathermap.org/img/wn/" + iconIdEl + "@2x.png";
     var imgCode = "<img src='" + link + "' alt='icon'>";
 
     var aqiBadgeElement = createBadge(results.data.current.pollution.aqius);
@@ -43,8 +75,9 @@ var getAQIInformation = function(results) {
         "<br><br><div class ='center-align'><div>Current AQI (US):</div><div> " + aqiBadgeElement + "</div></div>" +
         "<br><div class='pollutants'>Primary Pollutant: " + pollutantName +
         "</div><br></h3><h6>This is the AirVisual App at work.</h6></div>";
-
+    searchButtonHandler(cityFormatted);
 };
+
 
 // Creates a badge using materialize that indicates the AQI with color coding
 var createBadge = function(aqiValue) {
@@ -67,7 +100,7 @@ var createBadge = function(aqiValue) {
     return badgeCode;
 };
 
-// The API uses Celsius - so we want to convert this for our American users I suppose
+// The API uses Celsius - so we want to convert this to Fahrenheit
 
 function convertToF(celsius) {
     return celsius * 9 / 5 + 32;
@@ -96,5 +129,5 @@ var getPollutant = function(result) {
 
     return pollutantName;
 };
-getCurrentAirInfo();
+
 getCurrentAirInfo();
